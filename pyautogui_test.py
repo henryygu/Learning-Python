89120ef48b7c17a2c2b34bc13ed069e964ca168b
henryygu <32582668+henryygu@users.noreply.github.com>
2022-12-28 12:46:34 +0000

    pyaotuogui

---
+++ pyautogui_test.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+
+
+numMin = 3
+
+whiel(True):
+    x = 0
+    while(x < numMin):
+        time.sleep(60)
+        x+=1
+        for i in range(0,200):
+            pyautogui.moveTo(0,i*4)
+            pyautogui.moveTo(1,1)
+            for i in range(0,3):
+                pyautogui.press("shift")
