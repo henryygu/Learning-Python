465d5eabd8ef2db43557c67ca04f59304a076822
henryygu <32582668+henryygu@users.noreply.github.com>
2023-02-09 11:15:30 +0000

    looks finished

--- texttomp4/texttomp4.py
+++ texttomp4/texttomp4.py
@@ -1,60 +1,46 @@
 import os
 os.chdir('D:\\Users\\Henry\\Downloads\\github\\Learning-Python\\texttomp4') 
 
-
+import re
 from gtts import gTTS
-import os
-import cv2
+from pydub import AudioSegment
 from moviepy.editor import *
 
-def text_to_speech(text, filename):
-    # Convert the text to speech
-    tts = gTTS(text=text, lang='en')
-    # Save the speech to an audio file
-    tts.save(filename)
-
-def text_to_video(text, audio_filename, video_filename):
-    # Initialize the video file with a size and fps
-    video = cv2.VideoWriter(video_filename, cv2.VideoWriter_fourcc(*'mp4v'), 20, (1920, 1080))
-
-    # Split the text into lines
-    lines = text.split("\n")
-    # Calculate the number of lines
-    n_lines = len(lines)
-
-    # Load the audio file
-    audio = AudioFileClip(audio_filename)
-
-    # Get the duration of the audio
-    duration = audio.duration
-
-    # For each line of text
-    for i, line in enumerate(lines):
-        # Create an image with the line of text
-        img = np.zeros((1080, 1920, 3), np.uint8)
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img, line, (50, int(1080/2)), font, 4, (255, 255, 255), 8, cv2.LINE_AA)
-
-        # Write the image to the video file
-        video.write(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-
-    # Release the video file
-    video.release()
-
-    # Create a video clip from the audio and the video
-    video_clip = VideoFileClip(video_filename)
-    audio_clip = AudioFileClip(audio_filename)
-    final_clip = video_clip.set_audio(audio_clip)
-
-    # Write the final video to file
-    final_clip.write_videofile(video_filename)
-
-# Read the text file
-with open("input.txt", "r") as file:
+# Load the text file
+with open("text.txt", "r") as file:
     text = file.read()
 
-# Convert the text to speech and save it as an audio file
-text_to_speech(text, "speech.mp3")
+# Split the text into sentences based on new line or full stops
+sentences = re.split(r'[.\n]', text)
 
-# Create a video file that displays the text being read, synchronized with the audio
-text_to_video(text, "speech.mp3", "video.mp4")
+# Create a list to store the audio and video files
+audio_files = []
+video_files = []
+
+# Convert each sentence into a text to speech mp3 file and video file
+for i, sentence in enumerate(sentences):
+    # Convert the sentence into an mp3 file using gTTS
+    tts = gTTS(text=sentence, lang='en')
+    tts.save(f"sentence_{i}.mp3")
+    
+    # Load the mp3 file into a pydub AudioSegment object
+    audio = AudioSegment.from_file(f"sentence_{i}.mp3", format="mp3")
+    
+    # Create a video file with the sentence text
+    video = TextClip(sentence, font="Arial", fontsize=24, color='white')
+    video = video.set_duration(audio.duration_seconds)
+    video.write_videofile(f"sentence_{i}.mp4",fps=24)
+    
+    # Append the audio and video files to the list
+    audio_files.append(AudioFileClip(f"sentence_{i}.mp3"))
+    video_files.append(VideoFileClip(f"sentence_{i}.mp4"))
+
+# Concatenate all the audio and video files into one final video file
+final_video = concatenate_videoclips(video_files, method="compose")
+final_audio = concatenate_audioclips(audio_files)
+
+# Overlay the audio on top of the video
+final_video = final_video.set_audio(final_audio)
+
+# Write the final video file
+final_video.write_videofile("final_video.mp4")
