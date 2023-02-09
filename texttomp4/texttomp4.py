d1df06478db6d4a0bcd4fb3578e8e71ebe4ac4f2
henryygu <32582668+henryygu@users.noreply.github.com>
2023-02-09 12:45:09 +0000

    small fixes

--- texttomp4/texttomp4.py
+++ texttomp4/texttomp4.py
@@ -1,46 +1,60 @@
-import os
-os.chdir('D:\\Users\\Henry\\Downloads\\github\\Learning-Python\\texttomp4') 
-
 import re
+import os
 from gtts import gTTS
 from pydub import AudioSegment
 from moviepy.editor import *
 
-# Load the text file
-with open("text.txt", "r") as file:
-    text = file.read()
 
-# Split the text into sentences based on new line or full stops
-sentences = re.split(r'[.\n]', text)
+os.chdir('D:\\Users\\Henry\\Downloads\\github\\Learning-Python\\texttomp4') 
 
-# Create a list to store the audio and video files
-audio_files = []
-video_files = []
+# Loop through all the text files in the folder
+for filename in os.listdir():
+    if filename.endswith(".txt"):
+    # Load the text file
+        with open(os.path.join(filename), "r") as file:
+            text = file.read()
 
-# Convert each sentence into a text to speech mp3 file and video file
-for i, sentence in enumerate(sentences):
-    # Convert the sentence into an mp3 file using gTTS
-    tts = gTTS(text=sentence, lang='en')
-    tts.save(f"sentence_{i}.mp3")
-    
-    # Load the mp3 file into a pydub AudioSegment object
-    audio = AudioSegment.from_file(f"sentence_{i}.mp3", format="mp3")
-    
-    # Create a video file with the sentence text
-    video = TextClip(sentence, font="Arial", fontsize=24, color='white')
-    video = video.set_duration(audio.duration_seconds)
-    video.write_videofile(f"sentence_{i}.mp4",fps=24)
-    
-    # Append the audio and video files to the list
-    audio_files.append(AudioFileClip(f"sentence_{i}.mp3"))
-    video_files.append(VideoFileClip(f"sentence_{i}.mp4"))
+        # Split the text into sentences based on new line or full stops
+        sentences = re.split(r'[.\n]', text)
+        
+        # Create a list to store the audio and video files
+        audio_files = []
+        video_files = []
+        
+        # Convert each sentence into a text to speech mp3 file and video file
+        for i, sentence in enumerate(sentences):
+            if len(sentence)!=0:
+                # Convert the sentence into an mp3 file using gTTS
+                tts = gTTS(text=sentence, lang='en')
+                tts.save(f"sentence_{i}.mp3")
+                
+                # Load the mp3 file into a pydub AudioSegment object
+                audio = AudioSegment.from_file(f"sentence_{i}.mp3", format="mp3")
+                
+                # Create a video file with the sentence text
+                video = TextClip(sentence, font="Arial", fontsize=24, color='white')
+                video = video.set_duration(audio.duration_seconds)
+                video.write_videofile(f"sentence_{i}.mp4",fps=24)
+                video.close()
 
-# Concatenate all the audio and video files into one final video file
-final_video = concatenate_videoclips(video_files, method="compose")
-final_audio = concatenate_audioclips(audio_files)
+                # Append the audio and video files to the list
+                audio_files.append(AudioFileClip(f"sentence_{i}.mp3"))
+                video_files.append(VideoFileClip(f"sentence_{i}.mp4"))
+        
+        # Concatenate all the audio and video files into one final video file
+        final_video = concatenate_videoclips(video_files, method="compose")
+        final_audio = concatenate_audioclips(audio_files)
+        
+        # Overlay the audio on top of the video
+        final_video = final_video.set_audio(final_audio)
+        
+        # Write the final video file with the same name as the input text file
+        final_video.write_videofile(f"{os.path.splitext(filename)[0]}_final_video.mp4")
+        final_video.close() # close the final video file after saving it
 
-# Overlay the audio on top of the video
-final_video = final_video.set_audio(final_audio)
 
-# Write the final video file
-final_video.write_videofile("final_video.mp4")
+        # Delete the intermediate files
+        for i in range(len(sentences)):
+            if len(sentence)!=0:
+                os.remove(f"sentence_{i}.mp3")
+                os.remove(f"sentence_{i}.mp4")
