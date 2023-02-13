a0ee13aaf235a647a850476099b74b0057334783
henryygu <32582668+henryygu@users.noreply.github.com>
2023-02-13 18:04:05 +0000

    Update texttomp4.py

--- texttomp4/texttomp4.py
+++ texttomp4/texttomp4.py
@@ -5,7 +5,6 @@ from pydub import AudioSegment
 from moviepy.editor import *
 import cv2
 import numpy as np
-import soundfile as sf
 from io import BytesIO
 
 def remove_spaces(sentence):
@@ -14,6 +13,14 @@ def remove_spaces(sentence):
 
 os.chdir('D:\\Users\\Henry\\Downloads\\github\\Learning-Python\\texttomp4') 
 folder = "Files"
+
+existingfiles = os.listdir()
+mp3_or_mp4_files = [f for f in existingfiles if f.endswith(".mp3") or f.endswith(".mp4")]
+i_values = [int(re.search(r"sentence_(\d+)", f).group(1)) for f in mp3_or_mp4_files]
+# find the highest value of i
+highest_i = max(i_values)
+
+
 # Loop through all the text files in the folder
 for filename in os.listdir(folder):
     print(filename)
@@ -33,39 +40,42 @@ for filename in os.listdir(folder):
         # Convert each sentence into a text to speech mp3 file and video file
         for i, sentence in enumerate(sentences):
             print(f'{i} out of {len(sentences)}]' )
-            if len(sentence)!=0:
-                print("y")
-                # Convert the sentence into an mp3 file using gTTS
-                tts = gTTS(text=sentence, lang='en')
-                tts.save(f"sentence_{i}.mp3")
+            if i >= highest_i: #sometimes script fails, this will allow you to not have to redo if files already exists
+                continue
+            else:
+                if len(sentence)!=0:
+                    print("y")
+                    # Convert the sentence into an mp3 file using gTTS
+                    tts = gTTS(text=sentence, lang='en')
+                    tts.save(f"sentence_{i}.mp3")
 
-                # Load the mp3 file into a pydub AudioSegment object
-                audio = AudioSegment.from_file(f"sentence_{i}.mp3", format="mp3")
-                
-                # Create a video file with the sentence text using OpenCV
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                font_scale = 4
-                thickness = 2
-                text_size = cv2.getTextSize(sentence, font, font_scale, thickness)[0]
-                text_width, text_height = text_size[0], text_size[1]
-                height = text_height + 50
-                if text_width>1200:
-                    width = 1280
-                else:
-                    width = text_width + 50
-                image = np.zeros((height, width, 3), np.uint8)
-                cv2.putText(image, sentence, (25, height//2), font, font_scale, (255, 255, 255), thickness)
-                video_path = f"sentence_{i}.mp4"
-                video_fps = 24
-                fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-                out = cv2.VideoWriter(video_path, fourcc, video_fps, (width, height))
-                for j in range(int(audio.duration_seconds*video_fps)):
-                    out.write(image)
-                out.release()
+                    # Load the mp3 file into a pydub AudioSegment object
+                    audio = AudioSegment.from_file(f"sentence_{i}.mp3", format="mp3")
+                    
+                    # Create a video file with the sentence text using OpenCV
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    font_scale = 4
+                    thickness = 2
+                    text_size = cv2.getTextSize(sentence, font, font_scale, thickness)[0]
+                    text_width, text_height = text_size[0], text_size[1]
+                    height = text_height + 50
+                    if text_width>1200:
+                        width = 1280
+                    else:
+                        width = text_width + 50
+                    image = np.zeros((height, width, 3), np.uint8)
+                    cv2.putText(image, sentence, (25, height//2), font, font_scale, (255, 255, 255), thickness)
+                    video_path = f"sentence_{i}.mp4"
+                    video_fps = 24
+                    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+                    out = cv2.VideoWriter(video_path, fourcc, video_fps, (width, height))
+                    for j in range(int(audio.duration_seconds*video_fps)):
+                        out.write(image)
+                    out.release()
 
-                # Append the audio and video files to the list
-                audio_files.append(AudioFileClip(f"sentence_{i}.mp3"))
-                video_files.append(VideoFileClip(f"sentence_{i}.mp4"))
+                    # Append the audio and video files to the list
+                    audio_files.append(AudioFileClip(f"sentence_{i}.mp3"))
+                    video_files.append(VideoFileClip(f"sentence_{i}.mp4"))
         
         # Concatenate all the audio and video files into one final video file
         final_video = concatenate_videoclips(video_files, method="compose")
