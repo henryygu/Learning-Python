70b6f63274d4f827c06270a03fc28a111f8487e0
henryygu <32582668+henryygu@users.noreply.github.com>
2023-02-16 12:31:21 +0000

    Black

--- Clean_downloads_folder_with_date.py
+++ Clean_downloads_folder_with_date.py
@@ -1,5 +1,5 @@
 ##hg
-#check modified date and create the path if not exist
+# check modified date and create the path if not exist
 
 from genericpath import isdir
 import os
@@ -8,36 +8,46 @@ import glob
 import shutil
 import argparse
 
-parser = argparse.ArgumentParser(description="Just an example",
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("-e", "--extensions", default=[],nargs='+',help="type in extensions to filter in format py Default filters everything")
-parser.add_argument("-l", "--directory", default="current",help="directory to run in")
-parser.add_argument("-d", "--datetime", default="yes",help="sort with datetime?")
+parser = argparse.ArgumentParser(
+    description="Just an example",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument(
+    "-e",
+    "--extensions",
+    default=[],
+    nargs="+",
+    help="type in extensions to filter in format py Default filters everything",
+)
+parser.add_argument("-l", "--directory", default="current", help="directory to run in")
+parser.add_argument("-d", "--datetime", default="yes", help="sort with datetime?")
 args = parser.parse_args()
 config = vars(args)
-#print(config)
+# print(config)
 
 print(config["extensions"])
 
-if config["directory"] =="current":
-    download = os.getcwd() 
+if config["directory"] == "current":
+    download = os.getcwd()
 else:
     download = config["directory"]
     print(download)
 
-files_to_move= [] 
+files_to_move = []
 os.chdir(download)
-#list_of_files  = os.listdir(os.curdir) #glob.glob("*") 
+# list_of_files  = os.listdir(os.curdir) #glob.glob("*")
 
-if config["extensions"] =="all":
-    split_types = list(set(os.path.splitext(file)[-1] for file in os.listdir(os.curdir)))
+if config["extensions"] == "all":
+    split_types = list(
+        set(os.path.splitext(file)[-1] for file in os.listdir(os.curdir))
+    )
 else:
     split_types = list(set(config["extensions"]))
-    if '.' not in split_types:
-        split_types = ['.'+item for item in split_types]
+    if "." not in split_types:
+        split_types = ["." + item for item in split_types]
 
 
-if config["datetime"] =="yes":
+if config["datetime"] == "yes":
     for file in os.listdir(os.curdir):
         if file.endswith(tuple(split_types)):
             if os.path.isfile(file):
@@ -45,28 +55,27 @@ if config["datetime"] =="yes":
                 print(file)
                 print(file_time.strftime("%Y-%m-%d"))
                 filename, file_ext = os.path.splitext(file)
-                file_ext_name = file_ext.replace(".","")
-                print(filename,file_ext,file_ext_name)
+                file_ext_name = file_ext.replace(".", "")
+                print(filename, file_ext, file_ext_name)
                 if not os.path.exists(str(file_time.strftime("%Y-%m-%d"))):
                     os.mkdir(str(file_time.strftime("%Y-%m-%d")))
-                    if not os.path.exists(str(file_time.strftime("%Y-%m-%d"))+'/'+file_ext_name):
-                        os.mkdir(str(file_time.strftime("%Y-%m-%d"))+'/'+file_ext_name)
-                shutil.move(file, str(file_time.strftime("%Y-%m-%d"))+'/'+file_ext_name)
+                    if not os.path.exists(
+                        str(file_time.strftime("%Y-%m-%d")) + "/" + file_ext_name
+                    ):
+                        os.mkdir(
+                            str(file_time.strftime("%Y-%m-%d")) + "/" + file_ext_name
+                        )
+                shutil.move(
+                    file, str(file_time.strftime("%Y-%m-%d")) + "/" + file_ext_name
+                )
             else:
                 for file in os.listdir(os.curdir):
                     if file.endswith(tuple(split_types)):
                         if os.path.isfile(file):
                             print(file)
                             filename, file_ext = os.path.splitext(file)
-                            file_ext_name = file_ext.replace(".","")
-                            print(filename,file_ext,file_ext_name)
+                            file_ext_name = file_ext.replace(".", "")
+                            print(filename, file_ext, file_ext_name)
                             if not os.path.exists(file_ext_name):
                                 os.mkdir(file_ext_name)
-                            shutil.move(file,file_ext_name)
-
-
-
-
-
-
-
+                            shutil.move(file, file_ext_name)
--- pyautogui_test.py
+++ pyautogui_test.py
@@ -4,13 +4,13 @@ import time
 
 numMin = 3
 
-while(True):
+while True:
     x = 0
-    while(x < numMin):
+    while x < numMin:
         time.sleep(60)
-        x+=1
-        for i in range(0,200):
-            pyautogui.moveTo(0,i*4)
-            pyautogui.moveTo(1,1)
-            for i in range(0,3):
+        x += 1
+        for i in range(0, 200):
+            pyautogui.moveTo(0, i * 4)
+            pyautogui.moveTo(1, 1)
+            for i in range(0, 3):
                 pyautogui.press("shift")
