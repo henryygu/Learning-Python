d8fbda5d46828a67bf92cd45911821b598bc7dc0
henryygu <32582668+henryygu@users.noreply.github.com>
2023-03-02 09:01:45 +0000

    initial

---
+++ outlook/outlook.py
@@ -0,0 +1,20 @@
+import win32com.client
+import pandas as pd
+
+outlook = win32com.client.Dispatch('Outlook.Application').GetNamespace('MAPI')
+inbox = outlook.GetDefaultFolder(6)  # "6" refers to the index of the inbox folder
+
+# Get all emails in the inbox
+emails = inbox.Items
+
+# Restrict to emails with subject line containing "Screen"
+restricted_emails = emails.Restrict("@SQL=urn:schemas:httpmail:subject LIKE '%Screen%'")
+
+# Create a DataFrame of email properties
+email_data = []
+for email in restricted_emails:
+    print(email.Subject)
+    print(email.SentOn)
+
+
+
