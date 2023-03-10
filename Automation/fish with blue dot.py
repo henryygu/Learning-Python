4386b3923c7a7193a8b3cf30cafdcef91f9dd40e
henryygu <32582668+henryygu@users.noreply.github.com>
2023-03-10 11:07:10 +0000

    image_matching

--- Automation/fish with blue dot.py
+++ Automation/fish with blue dot.py
@@ -4,14 +4,14 @@ import pyautogui as pg
 import os
 import time
 import random
-
+import keyboard
 
 os.chdir('D:\\Users\\Henry\\Downloads\\github\\Learning-Python\\Automation') 
 
 
 # list of image filenames to search for
 #image_filenames = ["target.png", "target2.png", "target3.png"]
-TARGET_FILENAMES = ["fish1.png"]
+TARGET_FILENAMES = ["fish.png"]#,"fish1.png"]
 Fish_area = "fish_area.png"
 bluedot = "bluedot.png"
 
@@ -24,14 +24,14 @@ count = 0
 
 
 
-
-
-
 while True: 
-    bluedot_loc = pg.locateOnScreen(bluedot, confidence=0.9)
+    if keyboard.is_pressed("q"):
+        break
+    bluedot_loc = pg.locateOnScreen(bluedot, confidence=0.9,region = (1747,799,339,111))
     if bluedot_loc == None:
         for target_filename in TARGET_FILENAMES:
-            target_location = pg.locateOnScreen(target_filename, confidence=0.9) #, region = boundaries_tuple)
+            print(f"looking for fish {target_filename}")
+            target_location = pg.locateOnScreen(target_filename, confidence=0.5,region=(1533,262,900,400))
             if target_location is not None:
                 print(f"Found {target_filename} at {target_location}")
                 pg.click(pg.center(target_location))
--- Automation/locate_and_click_refactor.py
+++ Automation/locate_and_click_refactor.py
@@ -1,78 +1,86 @@
-import cv2
+from PIL import ImageGrab, Image
 import numpy as np
-import pyautogui as pg
 import os
-import time
-import random
-import logging
-from threading import Thread
-
-logging.basicConfig(level=logging.INFO)
-
+import pyautogui as py
+import cv2
+import keyboard
 os.chdir("D:\\Users\\Henry\\Downloads\\github\\Learning-Python\\Automation")
 
-image_filenames = ["target.png", "target1.png"]
-harvest_filenames = ["harvestall.png", "harvestall2.png"]
-plant_filenames = ["plantall.png", "plantall2.png"]
+
+# Load target and bluedot images
+target_img = cv2.imread('fish1.png')
+bluedot_img = cv2.imread('bluedot.png')
 
 
-def find_targets(image_filenames, confidence=0.8):
-    """
-    Finds the locations of the target images and returns a list of tuples
-    containing the (x, y) coordinates of each location.
-    """
-    locations = []
-    for filename in image_filenames:
-        location = pg.locateOnScreen(filename, confidence=confidence)
-        if location is not None:
-            locations.append(location)
-    return locations
+while True:
+    if keyboard.is_pressed("q"):
+        break
+
+    # Take a screenshot of the screen
+    screen = np.array(ImageGrab.grab())
+
+    # Search for target image in screenshot
+    target_loc = cv2.matchTemplate(screen, target_img, cv2.TM_CCOEFF_NORMED)
+
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(target_loc)
+
+    print('best match %s' % str(max_loc))
+    print('best match %s' % str(max_val))
+
+    target_size_w = target_img.shape[1]/2
+    target_size_h = target_img.shape[0]/2
+    target_loc_center = (max_loc[0]+target_size_w, max_loc[1]+target_size_h)
+
+    py.click(target_loc_center)
 
 
-def click_targets(locations):
-    """
-    Clicks on all of the locations in the list of tuples containing the
-    (x, y) coordinates of the target images.
-    """
-    for location in locations:
-        center = pg.center(location)
-        pg.click(center)
 
 
-def find_and_click_targets(image_filenames, harvest_filenames, plant_filenames):
-    x, y = pg.position()
-    harvest_locations = find_targets(harvest_filenames)
-    plant_locations = find_targets(plant_filenames)
-
-    # search for each image and print its location if found
-    for filename in image_filenames:
-        target = pg.locateOnScreen(filename, confidence=0.8)
-        if target is not None:
-            logging.info(f"Found {filename} at {target}")
-            click_targets(harvest_locations)
-            pg.moveTo(x, y)
-            time.sleep(random.randint(5, 10))
-            click_targets(plant_locations)
-            pg.moveTo(x, y)
-        else:
-            logging.info(f"Did not find {filename}")
 
 
-def run_script():
-    while True:
-        try:
-            find_and_click_targets(image_filenames, harvest_filenames, plant_filenames)
-            time.sleep(random.randint(10, 40))
-        except Exception as e:
-            logging.error(e)
 
 
-if __name__ == "__main__":
-    threads = []
-    for i in range(5):
-        t = Thread(target=run_script)
-        t.daemon = True
-        t.start()
-        threads.append(t)
-    for t in threads:
-        t.join()
+
+
+
+
+while True:
+    # Take a screenshot of the screen
+    screen = np.array(ImageGrab.grab())
+
+    # Search for target image in screenshot
+    target_loc = cv2.matchTemplate(screen, target_img, cv2.TM_CCOEFF_NORMED)
+
+    # If target found, click on it and start searching for bluedot
+    if len(target_match[0]) > 0:
+        # Click on target using mouse events
+        target_x, target_y = target_loc[::-1][0]
+        print(f"Target found at ({target_x}, {target_y})")
+        # Add code to click on the target image here
+        
+        # Start searching for bluedot
+        while True:
+            # Take another screenshot of the screen
+            screen = np.array(ImageGrab.grab())
+            
+            # Convert the screenshot to grayscale
+            screen_gray = cv2.cvtColor(screen, cv2.COLOR_RGB2GRAY)
+            
+            # Search for bluedot image in screenshot
+            bluedot_loc = cv2.matchTemplate(screen_gray, bluedot_img, cv2.TM_CCOEFF_NORMED)
+            bluedot_thresh = 0.8
+            bluedot_match = np.where(bluedot_loc >= bluedot_thresh)
+            
+            # If bluedot found, click on it
+            if len(bluedot_match[0]) > 0:
+                bluedot_x, bluedot_y = bluedot_match[::-1][0]
+                print(f"Bluedot found at ({bluedot_x}, {bluedot_y})")
+                # Add code to click on the bluedot image here
+            # If bluedot not found, go back to searching for target
+            else:
+                print("Bluedot not found, searching for target again")
+                break
+    # If target not found, keep searching
+    else:
+        print("Target not found, searching again")
+        continue
